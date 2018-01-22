adb96a6074eeb60d29f1c8a1fcbc5505f465aef1
Magnaboy <magnaboyy@hotmail.com>
2018-01-22 14:54:48 +1100

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./wikipedia_logo.svg";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      random_article: {},
+      query: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.getRandom();
+  }
+
+  getRandom() {
+    console.log(`getRandom`);
+    fetch(`https://en.wikipedia.org/api/rest_v1/page/random/summary`)
+      .then(res => res.json())
+      .then(data => {
+        return this.setState({ random_article: data });
+      });
+  }
+  handleEvent = event => {
+    event.preventDefault();
+    this.searchArticle();
+  };
+  searchArticle() {
+    console.log(`Query: ${this.state.query}`);
+    fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${this.state.query}`)
+      .then(res => res.json())
+      .then(data => {
+        return this.setState({ random_article: data });
+      });
+  }
+
+  fetchRelated(title) {
+    console.log(title);
+    fetch(`https://en.wikipedia.org/api/rest_v1/page/related/${title}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        return this.setState({ random_article: data.pages[0] });
+      });
+  }
+
+  handleChange(event) {
+    console.log("handleChange");
+    this.setState({ query: event.target.value });
+  }
+
   render() {
+    const article = this.state.random_article;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header className="center">
+          <button
+            onClick={() => {
+              this.getRandom();
+            }}
+          >
+            Random
+          </button>
+          <img src={logo} alt="Wikipedia Logo" />
+          <form onSubmit={this.handleEvent}>
+            <input type="text" value={this.state.query} onChange={this.handleChange} />
+            <input type="submit" value="Search" />
+          </form>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* only render article if it exists */}
+        {article.thumbnail && (
+          <div>
+            <div id="article">
+              <h1>{article.title}</h1>
+              <h2>{article.description}</h2>
+              <div id="body">
+                <img src={article.thumbnail.source} alt={article.title} />
+                <p>{article.extract}</p>
+              </div>
+            </div>
+            <div id="tools">
+              <input type="button" onClick={() => window.open(`https://en.wikipedia.org/api/rest_v1/page/pdf/${article.title}`, "_blank")} value="Download Page as .PDF" />
+              <input type="button" onClick={() => this.fetchRelated(article.title)} value="Show a related article" />
+            </div>
+          </div>
+        )}
       </div>
     );
   }
